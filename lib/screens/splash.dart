43f205e55aa92ade8681ecf5f67f51583e48b322
Sagar Dhadke <peaceunique2003@gmail.com>
2025-05-07 11:23:46 +0530

    Added CircularProgressIndicator

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,12 +1,27 @@
-import 'package:flutter/material.dart';
+import '../exports/myExports.dart';
 
-class MySplash extends StatelessWidget {
+class MySplash extends StatefulWidget {
   const MySplash({super.key});
+
+  @override
+  State<MySplash> createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => UserOnBoard())
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0XFF2DB0F6),
+      backgroundColor: AppColors.primaryColor,
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -22,14 +37,24 @@ class MySplash extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(bottom: 24.0),
-              child: Text(
-                "Your shop’s best friend",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white),
+              child: Column(
+                spacing: 15,
+                children: [
+                  CircularProgressIndicator(
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(AppColors.primaryDark),
+                    backgroundColor: Colors.white,
+                  ),
+                  Text(
+                    "Your shop’s best friend",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  ),
+                ],
               ),
             )
           ],
